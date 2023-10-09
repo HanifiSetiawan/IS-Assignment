a185147ce0a3efdc263320ffa91a6c27d193c60a
MikArya <MikArya@gitlab.com>
2023-10-09 11:52:24 +0700

    better looking form

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,3 +1,8 @@
 <div>
-    <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
+    <title>Login Page</title>
+</div>
+
+<div>
+    <h1>Login</h1>
+    
 </div>
