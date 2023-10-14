473790483cf271ddfb1c8c94f24609dbc76a13b0
mikhaelarya <5025211062@mhs.its.ac.id>
2023-10-14 23:27:42 +0700

    tidying routes

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrangController;
 use App\Http\Controllers\LoginController;
 
-Route::get('/', [HomeController::class, 'index'])->middleware('auth');
-
-Route::get('/form', [OrangController::class, 'index']);
-Route::post('/simpan-data', [OrangController::class, 'simpanData'])->name('simpan-data');
-
 Route::get('/login', [LoginController::class, 'index']);
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'register'])->name('register');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [HomeController::class, 'index']);
+
+    Route::get('/form', [OrangController::class, 'index']);
+    Route::post('/form', [OrangController::class, 'simpanData'])->name('simpan-data');
+});
