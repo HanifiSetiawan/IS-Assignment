b55d588d00d7311e65070e07868256cf602b0102
mikhaelarya <5025211062@mhs.its.ac.id>
2023-10-14 23:11:34 +0700

    routes:register,homepage(auth only)

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,19 @@
 
 // routes/web.php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrangController;
 use App\Http\Controllers\LoginController;
+
+Route::get('/', [HomeController::class, 'index'])->middleware('auth');
 
 Route::get('/form', [OrangController::class, 'index']);
 Route::post('/simpan-data', [OrangController::class, 'simpanData'])->name('simpan-data');
 
 Route::get('/login', [LoginController::class, 'index']);
 Route::post('/login', [LoginController::class, 'login'])->name('login');
+
+Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
