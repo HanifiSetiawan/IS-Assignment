db678049f7f25ee701202969dfdb97c8abd56976
mikhaelarya <5025211062@mhs.its.ac.id>
2023-10-14 11:24:13 +0700

    new login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 <body>
-<form action="{{ route('login') }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ route('login') }}" method="POST" enctype="multipart/form-data">
         @csrf
+        <h1>Login</h1>
         <div class="form-group">
             <label for="email">Email</label>
             <input type="email" name="email" class="form-control">
@@ -18,5 +20,6 @@
         </div>
         <button type="submit" id="submitbutton" class="btn btn-primary">Login</button>
     </form>
+
 </body>
 </html>
