79a19c1bcb3cdb7d034b88c9bc39caa54b23bb5e
mikhaelarya <mikabel.arya@gmail.com>
2023-11-24 18:46:31 +0700

    update : home view to show sent requests + css

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DataRequest;
 use Auth;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index() {
-        $user = Auth::user()->name;
-        return view('home', ['user' => $user]);
+        $user = Auth::user();
+
+        $sentDataRequests = DataRequest::where('from', '=', $user->email)->get();
+
+        return view('home', ['user' => $user->name, 'sent' => $sentDataRequests]);
+    }
+
+    public function incoming() {
+
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,37 @@
 @include('navbar')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<link rel="stylesheet" href="{{ asset('css/home.css') }}">
+
 <div>
     <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
     <h1>Welcome to The App, {{ $user }}</h1>
+
+
+    <div class="boxing">
+        <h3>Incoming Requests</h3>
+        <hr>
+        Requests here
+    </div>
+
+    <div class="boxing">
+        <h3>Sent Requests</h3>
+        <hr>
+        <table class="table table-bordered table-wrap">
+            <thead>
+                <tr>
+                    <th>To</th>
+                    <th>Response</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    @foreach($sent as $item)
+                        <td> {{ $item->to }} </td>
+                        <td> {{ $item->state }} </td>
+                    @endforeach
+                </tr>
+            </tbody>
+        </table>
+        
+    </div>
 </div>
